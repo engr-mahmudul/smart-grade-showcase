79ccf9f821b657ad0b96b848cb6635cf06b7c47d
Mahmudul Hasan <dev.mahmudulhasan@gmail.com>
2024-01-16 04:02:33 +0200

    Hero Section Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 export default function App() {
-  return <h1 className="text-6xl text-red-700">This is app</h1>;
+  return (
+    <div className="bg-[#172227] font-[Inter] text-white">
+      <Navbar />
+      <HeroSection />
+    </div>
+  );
 }
